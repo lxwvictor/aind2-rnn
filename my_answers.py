acda8ee00419c2e18b6b51f61604e28f79e88bed
Li Xiaowei <lxwvictor@gmail.com>
2017-06-18 17:17:02 +0800

    completed coding, pending to run on AWS for all the code

--- my_answers.py
+++ my_answers.py
@@ -13,25 +13,84 @@ def window_transform_series(series,window_size):
     X = []
     y = []
 
-    # reshape each 
-    X = np.asarray(X)
-    X.shape = (np.shape(X)[0:2])
-    y = np.asarray(y)
-    y.shape = (len(y),1)
+    # reshape each
     
+    # set the default data type as int64, the final data type will follow the input data
+    X = np.asarray(X, dtype=np.int64)
+    y = np.asarray(y, dtype=np.int64)
+    
+    length = len(series)
+    for i in range(0, length - window_size):
+        # get eash slice
+        x = series[i:i+window_size]
+        # concatenate to the earlier X
+        X = np.concatenate((X,x))
+
+        y1 = series[i+window_size:i+window_size+1]
+        y = np.concatenate((y,y1))
+
+    # set the shape of X and y
+    X.shape = (int(len(X)/window_size), window_size)
+    y.shape = (len(y),1)
+
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(step_size, window_size):
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size,1)))
+    model.add(Dense(1, activation='linear'))
+
+    # build model using keras documentation recommended optimizer initialization
+    optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+    # compile the model
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+
+    # run your model!
+    model.fit(X_train, y_train, epochs=1000, batch_size=50, verbose=0)
     pass
 
 
 ### TODO: list all unique characters in the text and remove any non-english ones
 def clean_text(text):
-    # find all unique characters in the text
-
-
     # remove as many non-english characters and character sequences as you can 
+    text = text.replace('/', ' ')
+    text = text.replace('è', ' ')
+    text = text.replace('?', ' ')
+    text = text.replace('&', ' ')
+    text = text.replace('%', ' ')
+    text = text.replace('(', ' ')
+    text = text.replace(')', ' ')
+    text = text.replace('à', ' ')
+    text = text.replace('â', ' ')
+    text = text.replace('é', ' ')
+    text = text.replace('*', ' ')
+    text = text.replace('@', ' ')
+    text = text.replace('$', ' ')
+    text = text.replace('-', ' ')
+    text = text.replace('0', ' ')
+    text = text.replace('1', ' ')
+    text = text.replace('2', ' ')
+    text = text.replace('3', ' ')
+    text = text.replace('4', ' ')
+    text = text.replace('5', ' ')
+    text = text.replace('6', ' ')
+    text = text.replace('7', ' ')
+    text = text.replace('8', ' ')
+    text = text.replace('9', ' ')
+
+    unique_char = set(text)
+    #print(unique_char)
+    #print(len(unique_char))
+
+    word_list = text.split(" ")
+    #print(len(word_list))
+    unique_word = set(word_list)
+    #print(len(unique_word))
+        
+    # shorten any extra dead space created above
+    text = text.replace('  ',' ')
 
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -40,6 +99,11 @@ def window_transform_text(text,window_size,step_size):
     inputs = []
     outputs = []
     
-
+    length = len(text)
+    for i in range(0, int((length-window_size)/step_size)):
+        curStartLoc = i * step_size
+        curInputSlice = text[curStartLoc:curStartLoc+window_size]
+        inputs.append(curInputSlice)
+        outputs.append(text[curStartLoc+window_size:curStartLoc+window_size+1])
     
     return inputs,outputs
